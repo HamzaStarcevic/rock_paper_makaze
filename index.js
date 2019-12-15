27184e6b248a5b43f32915ee2288de803214c2dc
Hamza Starcevic <star.hamza2002@gmail.com>
2019-12-15 18:13:52 +0100

    Implemented most of the back end javascript

--- index.js
+++ index.js
@@ -10,4 +10,20 @@ function playerChoice() {
   let choice = pchoice.toUpperCase();
   return choice;
 }
-document.write(playerChoice());
+function game() {
+  let ch1, ch2;
+  ch2 = computerPlay();
+  ch1 = playerChoice();
+  if (ch1 === ch2) {
+    return "You both chose the same, it's a draw";
+  } else if (
+    (ch1 === "ROCK" && ch2 === "SCISSORS") ||
+    (ch1 === "PAPER" && ch2 === "ROCK") ||
+    (ch1 === "SCISSORS" && ch2 === "PAPER")
+  ) {
+    return `The computer chose ${ch2} so you win, ${ch1} beats ${ch2}`;
+  } else {
+    return `The computer chose ${ch2} so you lose, ${ch2} beats ${ch1}`;
+  }
+}
+document.write(game());
